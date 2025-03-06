a32bb5a54b74eb1916f88b9c0930b584228c6453
monica230 <monicamaltacouto@gmail.com>
2025-03-06 15:45:16 -0300

    create end movement

---
+++ src/controllers/movement.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import verifyBranch from "../middlewares/verifyBranch";
+import { MovementController } from "../controllers/movement.controller";
+import verifyDriver from "../middlewares/verifyDriver";
+
+const movementRouter = Router();
+
+const movementController = new MovementController();
+
+movementRouter.post("/", verifyBranch, movementController.createMovement);
+movementRouter.get("/", verifyBranch, movementController.listAllMovements);
+movementRouter.patch("/:id/start", verifyDriver, movementController.startMovement);
+movementRouter.patch("/:id/end", movementController.endMovement);
+
+export default movementRouter;
